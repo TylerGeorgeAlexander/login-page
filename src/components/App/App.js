22a37d52c837c53372895e02886bd11049b50a6f
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-06-30 14:33:06 -0700

    added desc to the App Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Dashboard from '../Dashboard/Dashboard';
-import Login from '../Login/Login';
-import Preferences from '../Preferences/Preferences';
-import useToken from './useToken';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Dashboard from "../Dashboard/Dashboard";
+import Login from "../Login/Login";
+import Preferences from "../Preferences/Preferences";
+import useToken from "./useToken";
 
 function App() {
-
   const { token, setToken } = useToken();
 
-  if(!token) {
-    return <Login setToken={setToken} />
+  if (!token) {
+    return <Login setToken={setToken} />;
   }
 
   return (
     <div className="wrapper">
-      <h1>Application</h1>
+      <h1>Application Page</h1>
+      <p>
+        This is the page displayed after your login information has been
+        accepted and you have a token stored in either localStorage or
+        sessionStorage.
+      </p>
       <BrowserRouter>
         <Switch>
           <Route path="/dashboard">
